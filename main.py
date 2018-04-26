482ee84ae8913cdca1d7a7ff3caa8d078b4af99f
Atul <jain.atul43@gmail.com>
2018-04-27 05:03:00 +0530

    changes in main

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ db = SQLAlchemy(app)
 
 db.init_app(app)
 
-host = 'http://localhost:5000/'
+# host = 'http://localhost:5000/'
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], include=['tasks'])
 
